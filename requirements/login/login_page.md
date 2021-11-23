b925bf3a57f75fae0a10bc7236398292eb993942
Miqueias Pires <miqueiasousa011@gmail.com>
2021-11-23 15:23:36 -0300

    ensure LoginPage loads with correct initial state

---
+++ requirements/login/login_page.md
@@ -0,0 +1,22 @@
+
+# Login Page
+
+> # Regras
+
+1. Os campos devem começar sem exibir mensagem de erro
+2 O botão de fazer login deve começar desabilitado
+3 Após digitar algo em um campo, a mensagem de erro só 4 deve sumir se o campo for válido
+5 Validar email quando o usuário digitar no campo
+6 Mostrar mensagem de erro se o email for inválido
+7 Remover mensagem de erro se o email for válido
+8 Validar senha quando o usuário digitar no campo
+9 Mostrar mensagem de erro se a senha for inválida
+10 Remover mensagem de erro se a senha for válida
+11 Habilitar o botão de fazer login se todos os campos forem válidos
+12 Desabilitar o botão de fazer login se algum campo for inválido
+13 Exibir loading no início da ação de login
+14 Autenticar usuário com email e senha informados no formulário
+15 Exibir mensagem de erro caso o login falhe
+16 Gravar o token do usuário no cache para mantê-lo conectado
+17 Redirecionar o usuário para tela de lista de enquetes
+18 Esconder loading no fim da ação de login
---
+++ test/ui/pages/login_page_test.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:fordev/ui/pages/pages.dart';
+
+void main() {
+  testWidgets('Should load with correct initial values',
+      (WidgetTester tester) async {
+    const loginPage = MaterialApp(home: LoginPage());
+
+    //manda renderizar a pagina de login
+    await tester.pumpWidget(loginPage);
+
+    //Encontrar todos os filhos do tipo text do campo de email
+    final emailTextChildren = find.descendant(
+      of: find.bySemanticsLabel('Email'),
+      matching: find.byType(Text),
+    );
+
+    // Se encontrar mais de um filho é porque o campo de email contém um erro
+    expect(
+      emailTextChildren,
+      findsOneWidget,
+      reason:
+          'Quando o textFormField contem apenas um filho, ele não contem erros, pois um dos filhos é o label text',
+    );
+
+    final passwordTextChildren = find.descendant(
+      of: find.bySemanticsLabel('Senha'),
+      matching: find.byType(Text),
+    );
+
+    expect(
+      passwordTextChildren,
+      findsOneWidget,
+    );
+
+    final button = tester.widget<ElevatedButton>(
+      find.byType(ElevatedButton),
+    );
+
+    expect(button.onPressed, equals(null));
+  });
+}
